1d3ec33113bdec5a2a7de5e6e2e991cabddf820a
richardsonadjei <adjei.kwadjo@gmail.com>
2024-01-13 00:05:21 +0000

    Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
 
 export default function App() {
+
   return (
-    <div className="text-danger">
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/sign-out' element={<SignOut />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
